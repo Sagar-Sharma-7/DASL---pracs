d7493ad6b7cdfcff8abc283faccf830a74d8d1d6
Sagar Sharma <78422995+Sagar-Sharma-7@users.noreply.github.com>
2025-05-05 23:24:12 +0530

    Update assign4.cpp

--- assign4.cpp
+++ assign4.cpp
@@ -1,232 +1,129 @@
-// C++ implementation of the double
-// threaded binary searighth tree
-
 #include <iostream>
 using namespace std;
 
-// Class of the Node
 class Node {
-	int lbit, rbit;
-	int value;
-	Node *left, *right;
-
 public:
-	// Constructor of the
-	// Node of the Tree
-	Node()
-	{
-		lbit = rbit = 0;
-		value = 0;
-		left = right = NULL;
-	}
-	friend class DTBT;
+    int data;
+    Node* left;
+    Node* right;
+    bool lThread;
+    bool rThread;
+
+    Node(int value) {
+        data = value;
+        left = right = nullptr;
+        lThread = rThread = true;  // Initially both threads are true
+    }
 };
 
-// Class of the Threaded
-// Binary search tree
-class DTBT {
-	Node* root;
+class ThreadedBST {
+private:
+    Node* root;
 
 public:
-	// Constructor of the
-	// Threaded of the Binary
-	// Search Tree
-	DTBT()
-	{
-		root = new Node();
+    ThreadedBST() {
+        root = nullptr;
+    }
 
-		// Initialise the dummy node
-		// to any random value of
-		// your choice.
-		root->value = 9999;
+    // Insert into TBST
+    void insert(int key) {
+        Node* newNode = new Node(key);
 
-		// Considering our whole
-		// tree is at left of
-		// dummy node
-		root->rbit = 1;
-		root->lbit = 0;
+        // First node becomes root
+        if (root == nullptr) {
+            root = newNode;
+            return;
+        }
 
-		// Consider your whole tree
-		// lies to the left of
-		// this dummy node.
-		root->left = root;
-		root->right = root;
-	}
-	void create();
-	void insert(int value);
-	void preorder();
-	Node* preorderSuccessor(Node*);
-	void inorder();
-	Node* inorderSuccessor(Node*);
+        Node* curr = root;
+        Node* parent = nullptr;
+
+        while (curr != nullptr) {
+            if (key == curr->data) {
+                cout << "Duplicate not allowed\n";
+                return;
+            }
+
+            parent = curr;
+
+            if (key < curr->data) {
+                if (!curr->lThread)
+                    curr = curr->left;
+                else
+                    break;
+            } else {
+                if (!curr->rThread)
+                    curr = curr->right;
+                else
+                    break;
+            }
+        }
+
+        if (key < parent->data) {
+            newNode->left = parent->left;
+            newNode->right = parent;
+            parent->lThread = false;
+            parent->left = newNode;
+        } else {
+            newNode->right = parent->right;
+            newNode->left = parent;
+            parent->rThread = false;
+            parent->right = newNode;
+        }
+    }
+
+    // Find leftmost node
+    Node* leftMost(Node* node) {
+        if (node == nullptr) return nullptr;
+        while (!node->lThread)
+            node = node->left;
+        return node;
+    }
+
+    // Inorder traversal using threads
+    void inorder() {
+        Node* curr = leftMost(root);
+        cout << "Inorder Traversal: ";
+        while (curr != nullptr) {
+            cout << curr->data << " ";
+
+            if (curr->rThread)
+                curr = curr->right;
+            else
+                curr = leftMost(curr->right);
+        }
+        cout << endl;
+    }
 };
 
-// Function to create the Binary
-// search tree
-void DTBT::create()
-{
-	int n = 9;
+int main() {
+    ThreadedBST tree;
+    int choice, value;
 
-	// Insertion of the nodes
-	this->insert(6);
-	this->insert(3);
-	this->insert(1);
-	this->insert(5);
-	this->insert(8);
-	this->insert(7);
-	this->insert(11);
-	this->insert(9);
-	this->insert(13);
-}
+    do {
+        cout << "\n--- Threaded BST Menu ---\n";
+        cout << "1. Insert Node\n";
+        cout << "2. Inorder Traversal\n";
+        cout << "3. Exit\n";
+        cout << "Enter your choice: ";
+        cin >> choice;
 
-// Function to insert the nodes
-// into the threaded binary
-// search tree
-void DTBT::insert(int data)
-{
-	// Condition to check if there
-	// is no node in the binary tree
-	if (root->left == root
-		&& root->right == root) {
+        switch (choice) {
+        case 1:
+            cout << "Enter value to insert: ";
+            cin >> value;
+            tree.insert(value);
+            break;
+        case 2:
+            tree.inorder();
+            break;
+        case 3:
+            cout << "Exiting.\n";
+            break;
+        default:
+            cout << "Invalid choice. Try again.\n";
+        }
+    } while (choice != 3);
 
-		Node* p = new Node();
-		p->value = data;
-		p->left = root->left;
-		p->lbit = root->lbit;
-		p->rbit = 0;
-		p->right = root->right;
-
-		// Inserting the node in the
-		// left of the dummy node
-		root->left = p;
-		root->lbit = 1;
-		return;
-	}
-
-	// New node
-	Node* cur = new Node;
-	cur = root->left;
-	while (1) {
-		// Condition to check if the
-		// data to be inserted is
-		// less than the current node
-		if (cur->value < data) {
-			Node* p = new Node();
-			p->value = data;
-			if (cur->rbit == 0) {
-				p->right = cur->right;
-				p->rbit = cur->rbit;
-				p->lbit = 0;
-				p->left = cur;
-
-				// Inserting the node
-				// in the right
-				cur->rbit = 1;
-				cur->right = p;
-				return;
-			}
-			else
-				cur = cur->right;
-		}
-
-		// Otherwise insert the node
-		// in the left of current node
-		if (cur->value > data) {
-			Node* p = new Node();
-			p->value = data;
-			if (cur->lbit == 0) {
-				p->left = cur->left;
-				p->lbit = cur->lbit;
-				p->rbit = 0;
-
-				// Pointing the right child
-				// to its inorder Successor
-				p->right = cur;
-				cur->lbit = 1;
-				cur->left = p;
-				return;
-			}
-			else
-				cur = cur->left;
-		}
-	}
-}
-
-// In Threaded binary search tree
-// the left pointer of every node
-// points to its Inorder predecessor,
-// whereas its right pointer points
-// to the Inorder Successor
-void DTBT::preorder()
-{
-	Node* c = root->left;
-
-	// Loop to traverse the tree in
-	// the preorder fashion
-	while (c != root) {
-		cout << " " << c->value;
-		c = preorderSuccessor(c);
-	}
-}
-
-// Function to find the preorder
-// Successor of the node
-Node* DTBT::preorderSuccessor(Node* c)
-{
-	if (c->lbit == 1) {
-		return c->left;
-	}
-	while (c->rbit == 0) {
-		c = c->right;
-	}
-	return c->right;
-}
-
-// In Threaded binary search tree
-// the left pointer of every node
-// points to its Inorder predecessor
-// whereas its right pointer points
-// to the Inorder Successor
-void DTBT::inorder()
-{
-	Node* c;
-	c = root->left;
-	while (c->lbit == 1)
-		c = c->left;
-
-	// Loop to traverse the tree
-	while (c != root) {
-		cout << " " << c->value;
-		c = inorderSuccessor(c);
-	}
-}
-
-// Function to find the inorder
-// successor of the node
-Node* DTBT::inorderSuccessor(Node* c)
-{
-	if (c->rbit == 0)
-		return c->right;
-	else
-		c = c->right;
-	while (c->lbit == 1) {
-		c = c->left;
-	}
-	return c;
-}
-
-// Driver Code
-int main()
-{
-	DTBT t1;
-
-	// Creation of the Threaded
-	// Binary search tree
-	t1.create();
-
-	cout << "Inorder Traversal of DTBST\n";
-	t1.inorder();
-
-	cout << "\nPreorder Traversal of DTBST\n";
-	t1.preorder();
-	return 0;
+    return 0;
 }
